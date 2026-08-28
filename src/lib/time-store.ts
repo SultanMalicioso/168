12e9752cd082d0260be4809bfb147ae54c3ddcb8
SultanMalicioso <maximofc2009@gmail.com>
2026-08-28 11:48:46 -0300

    Update time-store.ts

--- src/lib/time-store.ts
+++ src/lib/time-store.ts
@@ -3,7 +3,10 @@ import {
   addWeeks,
 } from "@/lib/week-utils";
 import { useEffect, useState } from "react";
-import { CLOUD_UPDATED_EVENT } from "@/lib/cloud-sync";
+import {
+  CLOUD_UPDATED_EVENT,
+  LOCAL_DATA_CHANGED_EVENT,
+} from "@/lib/cloud-sync";
 
 export type WeekKey = string;
 
